ebaba20e89c34eadcda58306ead1651fb4c051cc
Elena Rodina <rodina-hr@mail.ru>
2022-11-27 23:36:34 +0500

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите количество элементов массива: ");
+int count = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int [] arr = new int[count];
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+}
+Console.Write('['+string.Join(", ", arr) + ']');
